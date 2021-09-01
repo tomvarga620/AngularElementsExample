05abe9bbd9877c1baa3f0b0a48fa4b8a2bc1411c
tomvarga620 <tom.tomasvarga@gmail.com>
2021-09-01 21:36:06 +0200

    added example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { createCustomElement } from '@angular/elements';
 import { TestComponent } from './test/test.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-     TestComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TestComponent, Test2Component],
+  imports: [BrowserModule],
   providers: [],
-  entryComponents: [TestComponent],
+  entryComponents: [TestComponent, Test2Component],
 })
 export class AppModule {
   constructor(private injector: Injector) {}
@@ -23,6 +19,11 @@ export class AppModule {
       injector: this.injector,
     });
 
+    const test2Component = createCustomElement(Test2Component, {
+      injector: this.injector,
+    });
+
     customElements.define('test-component', testComponent);
+    customElements.define('test2-component', test2Component);
   }
 }
